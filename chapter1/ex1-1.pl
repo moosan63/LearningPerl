cfcf8786f2c39fe4f673aa1596a7512a17575df2
Ryo MURAKAMI <moosan63@gmail.com>
2013-10-28 23:11:16 +0900

    chapter1 finished

---
+++ chapter1/ex1-1.pl
@@ -0,0 +1,5 @@
+use strict;
+use warnings;
+use 5.018;
+
+say "Hello, World";
---
+++ chapter1/ex1-3.pl
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+my @lines = `perldoc -u -f atan2`;
+foreach (@lines) {
+    s/\w<([^>]+)>/\U$1/g;
+    print;
+}
